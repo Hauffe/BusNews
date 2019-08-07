da44b708398e2d76c7850127055fd71dd74285ab
Alexandre <alex_hauffe@hotmail.com>
2019-08-07 20:50:12 -0300

    - removing unnecessary import

--- src/main/java/com/hauffe/news/controller/NewsController.java
+++ src/main/java/com/hauffe/news/controller/NewsController.java
@@ -8,8 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.xml.ws.Service;
 import java.util.List;
 
 @RestController

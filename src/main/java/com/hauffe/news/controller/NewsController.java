9c5000ac6a87343f56d8f8cfaef7b9e82e95686e
Alexandre <alex_hauffe@hotmail.com>
2021-02-21 19:53:25 -0300

    - Introducing pdf validation

--- src/main/java/com/hauffe/news/controller/NewsController.java
+++ src/main/java/com/hauffe/news/controller/NewsController.java
@@ -5,7 +5,6 @@ import com.hauffe.news.model.BusNews;
 import com.hauffe.news.service.NewsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +17,10 @@ public class NewsController {
     private final NewsService service;
     private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
 
-    @Autowired
     public NewsController(NewsService service) {
         this.service = service;
     }
+
 
     @RequestMapping("")
     public String hello() {
--- src/main/java/com/hauffe/news/model/BusNews.java
+++ src/main/java/com/hauffe/news/model/BusNews.java
@@ -5,6 +5,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.Objects;
 
 @Entity
 @Table(name = "BUS_NEWS")
@@ -30,4 +31,22 @@ public class BusNews {
     @Column(name = "IMAGE_URL")
     @Getter @Setter private String imageURL;
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BusNews busNews = (BusNews) o;
+        return Objects.equals(id, busNews.id) &&
+                Objects.equals(title, busNews.title) &&
+                Objects.equals(date, busNews.date) &&
+                Objects.equals(content, busNews.content) &&
+                Objects.equals(link, busNews.link) &&
+                Objects.equals(imageURL, busNews.imageURL);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, date, content, link, imageURL);
+    }
 }

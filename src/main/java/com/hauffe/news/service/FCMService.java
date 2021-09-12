51a99559a1f3c563c5a078e1f76fe374f1b007ab
Alexandre <alex_hauffe@hotmail.com>
2021-09-12 17:51:53 -0300

    - Removing non necessary methods

--- src/main/java/com/hauffe/news/service/FCMService.java
+++ src/main/java/com/hauffe/news/service/FCMService.java
@@ -15,25 +15,12 @@ import java.util.concurrent.ExecutionException;
 public class FCMService {
 
     private static final Logger logger = LoggerFactory.getLogger(FCMService.class);
-    public void sendMessage(Map<String, String> data, PushNotificationRequest request)
-            throws InterruptedException, ExecutionException {
-        Message message = getPreconfiguredMessageWithData(data, request);
-        String response = sendAndGetResponse(message);
-        logger.info("Sent message with data. Topic: " + request.getTopic() + ", " + response);
-    }
 
     public void sendMessageWithoutData(PushNotificationRequest request)
             throws InterruptedException, ExecutionException {
         Message message = getPreconfiguredMessageWithoutData(request);
         String response = sendAndGetResponse(message);
         logger.info("Sent message without data. Topic: " + request.getTopic() + ", " + response);
-    }
-
-    public void sendMessageToToken(PushNotificationRequest request)
-            throws InterruptedException, ExecutionException {
-        Message message = getPreconfiguredMessageToToken(request);
-        String response = sendAndGetResponse(message);
-        logger.info("Sent message to token. Device token: " + request.getToken() + ", " + response);
     }
 
     private String sendAndGetResponse(Message message) throws InterruptedException, ExecutionException {
@@ -53,18 +40,8 @@ public class FCMService {
                 .setAps(Aps.builder().setCategory(topic).setThreadId(topic).build()).build();
     }
 
-    private Message getPreconfiguredMessageToToken(PushNotificationRequest request) {
-        return getPreconfiguredMessageBuilder(request).setToken(request.getToken())
-                .build();
-    }
-
     private Message getPreconfiguredMessageWithoutData(PushNotificationRequest request) {
         return getPreconfiguredMessageBuilder(request).setTopic(request.getTopic())
-                .build();
-    }
-
-    private Message getPreconfiguredMessageWithData(Map<String, String> data, PushNotificationRequest request) {
-        return getPreconfiguredMessageBuilder(request).putAllData(data).setTopic(request.getTopic())
                 .build();
     }
 
